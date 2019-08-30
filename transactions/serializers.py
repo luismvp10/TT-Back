15e680847c2aaa99a3d16bc74782251a0e84a443
Alexis9601 <humberto.9601@gmail.com>
2019-08-30 02:05:21 -0500

    Se corrigio el servicio de transacciones

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -5,11 +5,13 @@ class TransactionSerializer(serializers.ModelSerializer):
     id_transaction = serializers.ReadOnlyField()
     price = serializers.ReadOnlyField(allow_null=True)
     weight = serializers.ReadOnlyField(allow_null=True)
-    kind = serializers.ReadOnlyField(source='kind.name',allow_null=True)
-    country = serializers.ReadOnlyField(source='country.name',allow_null=True)
     section = serializers.ReadOnlyField(source='section.name',allow_null=True)
     year = serializers.ReadOnlyField(source='year.name',allow_null=True)
     month = serializers.ReadOnlyField(source='month.name',allow_null=True)
     class Meta:
         model = Transaction
-        fields=('id_transaction', 'price', 'weight', 'kind', 'country', 'section', 'year', 'month')
+        fields=('id_transaction', 'price', 'weight', 'kind', 'section', 'year', 'month')
+
+class CustomSerializer(serializers.Serializer):
+    country = serializers.CharField(max_length=50)
+    transacciones = TransactionSerializer(many=True)
